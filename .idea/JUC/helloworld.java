077b5d54b9f11a2c9d7ea0364ada77c29602029b
z1524 <1524285681@qq.com>
2018-11-11 14:57:45 +0800

    reentrantlock

---
+++ .idea/JUC/helloworld.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
+ public class helloworld{
+     public static void main(String[]args){
+        System.out.println("hello world");
+        //ReentrantLock
+         }
+}
